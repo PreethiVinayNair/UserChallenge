29a25dbeba446c88bb2e914513dcc99cbd331b91
Preethi Nair <preet.nair2004@gmail.com>
2020-04-23 12:02:35 +1200

    Revert "Name Change to the solution from RandomUserChallenge to UserChallenge"
    This reverts commit f5bbc0b46541233a14e257a2137fda40aaa4df69.

--- RandomUserChallenge/RandomUserChallenge/Pages/Error.cshtml.cs
+++ RandomUserChallenge/RandomUserChallenge/Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace UserChallenge.Pages
+namespace RandomUserChallenge.Pages
 {
   [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
   public class ErrorModel : PageModel
